fc4db0d0b3a5c3a66c81c4c606f6a7c84b568ef8
Lejla Špago <lejla.spago@edu.fit.ba>
2019-01-14 16:21:40 +0100

    ProjectTaskEntry

---
+++ src/Taskter.Core/Entities/ProjectTaskEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taskter.Core.Entities
+{
+    class ProjectTaskEntry
+    {
+        public int ProjectTaskId { get; set; }
+        public ProjectTask ProjectTask { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int durationInMin { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
