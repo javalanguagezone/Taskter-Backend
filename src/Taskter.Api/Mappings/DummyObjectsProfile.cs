3e17a6fb72b57af101cae2f62cfa565bf652ed19
Tarik Eminagic <tarik.eminagic@cegeka.com>
2019-01-18 15:23:28 +0100

    [tarik] added automapper

---
+++ src/Taskter.Api/Mappings/DummyObjectsProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Taskter.Api.Contracts;
+using Taskter.Core.Entities;
+
+namespace Taskter.Api.Mappings
+{
+    public class DummyObjectsProfile : Profile
+    {
+        public DummyObjectsProfile()
+        {
+            CreateMap<DummyInsertDto, Dummy>();
+        }
+    }
+}
