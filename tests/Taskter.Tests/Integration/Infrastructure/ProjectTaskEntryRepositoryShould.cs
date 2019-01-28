7241da07980f03eded3a7a1a9a44843e3860e220
DejanAcimovic <dacimovic1@etf.unsa.ba>
2019-01-28 15:44:06 +0100

    Small refactoring for repository test.

--- tests/Taskter.Tests/Integration/Infrastructure/ProjectTaskEntryRepositoryShould.cs
+++ tests/Taskter.Tests/Integration/Infrastructure/ProjectTaskEntryRepositoryShould.cs
@@ -21,24 +21,26 @@ namespace Taskter.Tests.Integration.Api
     [TestFixture]
     public class ProjectEntryRepositoryShould
     {
-        private TaskterDbContext _context;
+        private ProjectTaskEntryRepository _repository;
 
         [SetUp]
         public void SetUp()
         {
-            _context = new TaskterDbContext(new DbContextOptionsBuilder<TaskterDbContext>()
+            var _context = new TaskterDbContext(new DbContextOptionsBuilder<TaskterDbContext>()
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
                 .Options);
-
             _context.Database.EnsureCreated();
+
+            _repository = new ProjectTaskEntryRepository(_context);
         }
+
         [Test]
         public void AddEntry()
         {
-            ProjectTaskEntryRepository _repository = new ProjectTaskEntryRepository(_context);
-
             var newEntry = new ProjectTaskEntry(50, 2, 1, 50, new DateTime(2019, 2, 10), "Nasa nota");
+            
             _repository.AddTimeEntry(newEntry);
+            
             var result = _repository.GetProjectTaskEntriesByDate(2, 2019, 2, 10);
             result.Should().NotBeEmpty();
         }
@@ -46,10 +48,11 @@ namespace Taskter.Tests.Integration.Api
         [Test]
         public void AddOnlyOneEntryOnSpecifiedDay()
         {
-            ProjectTaskEntryRepository _repository = new ProjectTaskEntryRepository(_context);
             int numOfEnries = _repository.GetProjectTaskEntriesByDate(2, 2019, 2, 10).ToList().Count;
+            
             var newEntry = new ProjectTaskEntry(50, 2, 1, 50, new DateTime(2019, 2, 10), "Nasa nota");
             _repository.AddTimeEntry(newEntry);
+            
             var result = _repository.GetProjectTaskEntriesByDate(2, 2019, 2, 10);
             result.ToList().Count.Should().Be(numOfEnries+1);
         }
