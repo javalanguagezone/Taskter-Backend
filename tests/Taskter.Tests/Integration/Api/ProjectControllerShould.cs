1231655fd07ebc2c060309f6747d92b211c5ee15
Nermin <nermin.milisic@edu.fit.ba>
2019-02-06 14:50:23 +0100

    configured integration tests to use ConfigureTestServices method from test host, finished first integration test

--- tests/Taskter.Tests/Integration/Api/ProjectControllerShould.cs
+++ tests/Taskter.Tests/Integration/Api/ProjectControllerShould.cs
@@ -9,13 +9,10 @@ using Taskter.Tests.Helpers.Factories;
 using Microsoft.Extensions.DependencyInjection;
 using Taskter.Api.Contracts;
 using Taskter.Core.Entities;
-using Taskter.Core.Interfaces;
 using Taskter.Infrastructure.Data;
-using Taskter.Infrastructure.Repositories;
 using Taskter.Infrastructure.UserContext;
 using Taskter.Tests.Helpers.Extensions;
 using Microsoft.AspNetCore.TestHost;
-using System;
 
 namespace Taskter.Tests.Integration.Api
 {
@@ -25,11 +22,6 @@ namespace Taskter.Tests.Integration.Api
         private HttpClient _client;
         private ICurrentUserContext _currentUserContext;
         private TaskterDbContext _dbContext;
-
-        [SetUp]
-        public void SetUp()
-        {
-        }
 
         [Test]
         public async Task GetProjectsForCurrentUser_SeededTwoProjectsInTest_ReturnsAListOfThoseTwoSeededProjects()
@@ -45,26 +37,28 @@ namespace Taskter.Tests.Integration.Api
                     services.AddTransient<ICurrentUserContext>(x => _currentUserContext);
                     var sp = services.BuildServiceProvider();
                     _dbContext = sp.GetRequiredService<TaskterDbContext>();
-
                 });
             }).CreateClient();
             //
             //seed user
             _dbContext.Users.Add(new User("test1", "test 1", "test lastName", "admin", "http://google.com")
             { Id = _currentUserContext.UserId });
+            //seed client
+            var clientSeed = new Client("testClient") { Id = 20 };
+            _dbContext.Clients.Add(clientSeed);
             //seed projects
             var seedProjectsList = new List<Project>()
             {
-                new Project("test project 1", 1, null) {Id = 10},
-                new Project("test project 2", 1, null) {Id = 11}
+                new Project("test project 1", 20, null) {Id = 10, Client = clientSeed},
+                new Project("test project 2", 20, null) {Id = 11, Client = clientSeed}
             };
 
             var seedProjectsTaskList = new List<ProjectTask>()
             {
-                new ProjectTask("testTask1",10,false) {Id =20},
-                new ProjectTask("testTask2",10,false) {Id =21},
-                new ProjectTask("testTask1",11,false) {Id =22},
-                new ProjectTask("testTask2",11,false) {Id =23}
+                new ProjectTask("testTask1",10,false) {Id = 20},
+                new ProjectTask("testTask2",10,false) {Id = 21},
+                new ProjectTask("testTask1",11,false) {Id = 22},
+                new ProjectTask("testTask2",11,false) {Id = 23}
             };
             _dbContext.Projects.AddRange(seedProjectsList);
             _dbContext.ProjectTasks.AddRange(seedProjectsTaskList);
@@ -73,19 +67,9 @@ namespace Taskter.Tests.Integration.Api
             _dbContext.UsersProjects.Add(new UserProject(3, 11));
             _dbContext.SaveChanges();
             //test
-            _currentUserContext.UserId.Should().Be(3);
             var result = await _client.GetProjectsForCurrentUser();
-            IEnumerable<ProjectDTO> seedsDTO = new List<ProjectDTO>();
-            try
-            {
-                seedsDTO = seedProjectsList.ToDTOList();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            result.Should().BeEquivalentTo(seedsDTO);
-            var hw = "hw";
+            var seedsDto = seedProjectsList.ToDTOList();
+            result.Should().BeEquivalentTo(seedsDto);
         }
     }
 }
