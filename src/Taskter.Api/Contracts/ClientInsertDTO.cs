5c66ef88335f9e85280b06d213cc6b6ecb7344a3
selimhuskic <34183805+selimhuskic@users.noreply.github.com>
2019-02-15 09:46:38 +0100

    Small changes in the API

--- src/Taskter.Api/Contracts/ClientInsertDTO.cs
+++ src/Taskter.Api/Contracts/ClientInsertDTO.cs
@@ -4,7 +4,7 @@ namespace Taskter.Api.Contracts
 {
     public class ClientInsertDTO
     {
-        [Required]
+       
         public int ClientId { get; set; }
         [Required]
         public string Name { get; set; }
