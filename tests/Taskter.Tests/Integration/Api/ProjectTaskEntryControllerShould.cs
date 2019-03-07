61735822fc2e888ff107c4a899f658e018b7f48e
Nermin <noxyb0x@users.noreply.github.com>
2019-03-07 19:47:43 +0100

    Edit task entry1 (#16)
    * added a common setup method available for everyone to use in projectControllerShould
    
    * integration test rework attempt
    
    * Microsoft.AspNetCore.TestHost added
    
    * possible workaround for integration test problems
    
    * small changes
    
    * try catch
    
    * configured integration tests to use ConfigureTestServices method from test host, finished first integration test
    
    * test maintainability for project repository unit
    
    * finished integration test for ProjectTaskEntry
    
    * added an integration test for projectTaskEntry post request, removed dummies, cleaned up code formating a bit
    
    * removed inital seeds except for users, maintained some tests
    
    * edit task entry finished
    
    * added tests for GetTaskEntryById and UpdateTaskEntry
    
    * changed name of test method
    
    *  added 2 test for ProjectTaskEntry repository

--- tests/Taskter.Tests/Integration/Api/ProjectTaskEntryControllerShould.cs
+++ tests/Taskter.Tests/Integration/Api/ProjectTaskEntryControllerShould.cs
@@ -14,6 +14,7 @@ using Taskter.Tests.Helpers.Factories;
 using Taskter.Tests.Helpers.Extensions;
 using Taskter.Api.Contracts;
 using FluentAssertions;
+using System.Net;
 
 namespace Taskter.Tests.Integration.Api
 {
@@ -101,6 +102,97 @@ namespace Taskter.Tests.Integration.Api
 
             var listOfEntries = _dbContext.ProjectTaskEntries.ToList();
             listOfEntries.Count.Should().Be(2);
+        }
+
+        [Test]
+        public async Task GetProjectTaskEntryByIdAsync_AddedTwoTaskEntries_ReturnsEntryWithCorrectId()
+        {
+            _client = new IntegrationWebApplicationFactory<Startup>().WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    var serviceDesc = services.FirstOrDefault(desc => desc.ServiceType == typeof(ICurrentUserContext));
+                    services.Remove(serviceDesc);
+                    _currentUserContext = new CurrentUserContext() { UserId = 3 };
+                    services.AddTransient<ICurrentUserContext>(x => _currentUserContext);
+                    var sp = services.BuildServiceProvider();
+                    _dbContext = sp.GetRequiredService<TaskterDbContext>();
+                });
+            }).CreateClient();
+
+            _dbContext.Users.Add(new User("test1", "test 1", "test lastName", "admin", "http://google.com")
+            { Id = _currentUserContext.UserId });
+            var clientSeed = new Client("testClient") { Id = 20 };
+            _dbContext.Clients.Add(clientSeed);
+            var seedProjectsList = new List<Project>()
+            {
+                new Project("test project 1", 20, null) {Id = 10, Client = clientSeed},
+                new Project("test project 2", 20, null) {Id = 11, Client = clientSeed}
+            };
+            _dbContext.AddRange(seedProjectsList);
+            var seedProjectsTaskList = new List<ProjectTask>()
+            {
+                new ProjectTask("testTask1",10,false) {Id = 20, Project=seedProjectsList[0]},
+                new ProjectTask("testTask2",10,false) {Id = 21, Project=seedProjectsList[1]}
+
+            };
+            var seedProjectsTaskEntryList = new List<ProjectTaskEntry>()
+            {
+                new ProjectTaskEntry(3,20,30,DateTime.Now, "Notee") {Id = 10, ProjectTask=seedProjectsTaskList[0]},
+                new ProjectTaskEntry(3,21,50,DateTime.Now,"Notee") {Id = 11, ProjectTask=seedProjectsTaskList[1]}
+            };
+            _dbContext.ProjectTaskEntries.AddRange(seedProjectsTaskEntryList);
+            _dbContext.SaveChanges();
+
+            var result = await _client.GetProjectTaskEntriesByIdAsync(10);
+            var pteUpdateDTO = seedProjectsTaskEntryList[0].ToUpdateDTO();
+            result.Should().BeEquivalentTo(pteUpdateDTO);
+        }
+
+        [Test]
+        public async Task UpdateEntry_AddedOneEntryChangedValuesOfEntry_ReturnsNoContentAndUpdatedEntry()
+        {
+            _client = new IntegrationWebApplicationFactory<Startup>().WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    var serviceDesc = services.FirstOrDefault(desc => desc.ServiceType == typeof(ICurrentUserContext));
+                    services.Remove(serviceDesc);
+                    _currentUserContext = new CurrentUserContext() { UserId = 3 };
+                    services.AddTransient<ICurrentUserContext>(x => _currentUserContext);
+                    var sp = services.BuildServiceProvider();
+                    _dbContext = sp.GetRequiredService<TaskterDbContext>();
+                });
+            }).CreateClient();
+
+            _dbContext.Users.Add(new User("test1", "test 1", "test lastName", "admin", "http://google.com")
+            { Id = _currentUserContext.UserId });
+            var clientSeed = new Client("testClient") { Id = 20 };
+            _dbContext.Clients.Add(clientSeed);
+            var seedProjectsList = new List<Project>()
+            {
+                new Project("test project 1", 20, null) {Id = 10, Client = clientSeed},
+                new Project("test project 2", 20, null) {Id = 11, Client = clientSeed}
+            };
+            _dbContext.AddRange(seedProjectsList);
+            var seedProjectsTaskList = new List<ProjectTask>()
+            {
+                new ProjectTask("testTask1",10,false) {Id = 20, Project=seedProjectsList[0]},
+                new ProjectTask("testTask2",10,false) {Id = 21, Project=seedProjectsList[1]}
+
+            };
+
+            var taskEntry = new ProjectTaskEntry(3, 20, 30, DateTime.Now, "Notee") { Id = 10, ProjectTask = seedProjectsTaskList[0] };
+                       
+            _dbContext.ProjectTaskEntries.Add(taskEntry);
+            _dbContext.SaveChanges();
+
+            taskEntry.DurationInMin = 100;
+            var result = await _client.UpdateTaskEntry(taskEntry.ToUpdateDTO());
+            var updatedTaskEntry = await _client.GetProjectTaskEntriesByIdAsync(10);
+
+            updatedTaskEntry.durationInMin.Should().Be(100);
+            result.StatusCode.Should().BeEquivalentTo(System.Net.HttpStatusCode.NoContent);
         }
 
     }
